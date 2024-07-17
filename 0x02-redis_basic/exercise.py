9855540c63e42b825c7b2d67cb03fa5d51f7267d
reela <sudanrela123@gmail.com>
2024-07-17 13:13:33 +0000

    tst

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -32,17 +32,17 @@ def call_history(method: Callable) -> Callable:
 
 
 def replay(fn: Callable):
-    '''display the history of calls of a particular function.'''
-    r = redis.Redis()
+    """display the history of call"""
+    ra = redis.Redis()
     func_name = fn.__qualname__
-    c = r.get(func_name)
+    c = ra.get(func_name)
     try:
         c = int(c.decode("utf-8"))
     except Exception:
         c = 0
     print("{} was called {} times:".format(func_name, c))
-    inputs = r.lrange("{}:inputs".format(func_name), 0, -1)
-    outputs = r.lrange("{}:outputs".format(func_name), 0, -1)
+    inputs = ra.lrange("{}:inputs".format(func_name), 0, -1)
+    outputs = ra.lrange("{}:outputs".format(func_name), 0, -1)
     for inp, outp in zip(inputs, outputs):
         try:
             inp = inp.decode("utf-8")
@@ -56,35 +56,35 @@ def replay(fn: Callable):
 
 
 class Cache:
-    '''declares a Cache redis class'''
+    """declare Cache redis class"""
     def __init__(self):
-        '''upon init to store an instance and flush'''
+        """ store an instance and flush"""
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
     @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''takes a data argument and returns a string'''
-        rkey = str(uuid4())
-        self._redis.set(rkey, data)
-        return rkey
+        """take a data argument and returns a string"""
+        rky = str(uuid4())
+        self._redis.set(rky, data)
+        return rky
 
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        '''convert the data back to the desired format'''
+        """take data back to the desired format"""
         value = self._redis.get(key)
         if fn:
             value = fn(value)
         return value
 
     def get_str(self, key: str) -> str:
-        '''parametrize Cache.get with correct conversion function'''
+        """parametrize Cache.get"""
         value = self._redis.get(key)
         return value.decode("utf-8")
 
     def get_int(self, key: str) -> int:
-        '''parametrize Cache.get with correct conversion function'''
+        """parametrize Cache.get"""
         value = self._redis.get(key)
         try:
             value = int(value.decode("utf-8"))
