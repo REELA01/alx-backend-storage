9ee2b01e15b9da5496445bdd2bb452669744b40d
reela <sudanrela123@gmail.com>
2024-07-17 13:24:18 +0000

    tst

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -18,8 +18,10 @@ def count_url_access(method):
         cached_data = store.get(cached_key)
         if cached_data:
             return cached_data.decode("utf-8")
+
         count_key = "count:" + url
         html = method(url)
+
         store.incr(count_key)
         store.set(cached_key, html)
         store.expire(cached_key, 10)
